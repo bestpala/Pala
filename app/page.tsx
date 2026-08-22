bb290a4ad7b76447e8fb9566335326905aa266ef
bestpala <62168821+bestpala@users.noreply.github.com>
2026-08-22 11:43:00 +0800

    feat(home): redesign app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Hero } from "@/components/home/Hero";
 import { IdentityIntro } from "@/components/home/IdentityIntro";
-import { ThemeEntries } from "@/components/home/ThemeEntries";
-import { ConferenceHighlight } from "@/components/home/ConferenceHighlight";
 import { FeaturedWorks } from "@/components/home/FeaturedWorks";
-import { RecentPosts } from "@/components/home/RecentPosts";
-import { CommunityTeaser } from "@/components/home/CommunityTeaser";
 import { TimelineTeaser } from "@/components/home/TimelineTeaser";
+import { ConferenceHighlight } from "@/components/home/ConferenceHighlight";
+import { CommunityTeaser } from "@/components/home/CommunityTeaser";
+import { ThemeEntries } from "@/components/home/ThemeEntries";
+import { RecentPosts } from "@/components/home/RecentPosts";
 import { ContactTeaser } from "@/components/home/ContactTeaser";
 
 export default function HomePage() {
@@ -14,12 +14,13 @@ export default function HomePage() {
       <Hero />
       <IdentityIntro />
       <FeaturedWorks />
+      <TimelineTeaser />
       <ConferenceHighlight />
+      <CommunityTeaser />
       <ThemeEntries />
       <RecentPosts />
-      <CommunityTeaser />
-      <TimelineTeaser />
       <ContactTeaser />
     </>
   );
 }
+
